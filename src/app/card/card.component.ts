9795a3a4f7ceaa2a2ba82b11179710d108859a08
Ergun Polat <epolat@rocketlawyer.co.uk>
2017-02-26 13:52:25 +0000

    update

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'tc-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.css']
 })
 export class CardComponent implements OnInit {
 
